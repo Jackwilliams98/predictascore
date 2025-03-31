223aeb93489116af2c0a91f4967c94c8c3bd4b0e
jackwilliams98 <jackwilliams98@hotmail.com>
2025-03-31 08:18:11 +0100

    remove public facing users

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import React from "react";
 import { getUsers } from "@/lib/userAPI";
 
 export default async function Home() {
-  const users = await getUsers();
+  // const users = await getUsers();
 
   return (
     <div>
       Home page
-      {users.map((user) => (
+      {/* {users.map((user) => (
         <div key={user.id}>
           <h2>{user.name}</h2>
           <p>{user.email}</p>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
